d347ee39b042541206b5e310e62f020dc3b56e6b
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-12-27 01:55:12 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{razor,html}',
+    './{Layout,Pages}/**/*.{razor,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
