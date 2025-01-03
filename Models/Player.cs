290ae0e4ae79550a0e684ab00b477b3a9541d0d9
joserafael0160 <joserafaelperezrivero113@gmail.com>
2025-01-03 15:23:15 -0500

    Add: Player class

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,1 +1,14 @@
 namespace AnimeMaze.Models;
+
+public class Player
+{
+    public (int RowPosition, int ColPosition) Position { get; set; }
+    public Hero? HeroSelected { get; set; }
+
+    public Player(int rowPosition, int colPosition)
+    {
+        Position = (rowPosition, colPosition);
+    }
+}
+
+ 
--- obj/Debug/net8.0/Anime Maze.AssemblyInfo.cs
+++ obj/Debug/net8.0/Anime Maze.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anime_Maze")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e5b037416bca137a0932671ab2d856c2ea6abd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ac66c7d53b6753a4e545a8be842e1b43396074")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anime_Maze")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anime_Maze")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
