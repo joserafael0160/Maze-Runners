2291aae27c0483c7efae76de3194c2888631cb78
jose_rafael016 <joserafaelperezr@icloud.com>
2025-03-10 22:34:29 -0400

    changes

--- Data/TrapData.cs
+++ Data/TrapData.cs
@@ -7,61 +7,61 @@ namespace AnimeMaze.Data;
 
 public static class TrapData
 {
-    public static List<Trap> Traps =
-    [
-        new Trap(
-                "Trampa de Veneno",
-                "Un gas venenoso del anime 'Naruto' que reduce la salud del jugador en 20 puntos.",
-                "/images/Traps/Green Smoke.png",
-                player => player.Health -= 20
-            ),
-            new Trap(
-                "Trampa de Parálisis",
-                "Electricidad del 'Raikiri' del anime 'Bleach' que inmoviliza al jugador por un turno.",
-                "/images/Traps/Thunder.png",
-                player =>
-                {
-                    if (player != null && player.HeroSelected != null)
-                    {
-                        player.AddTemporaryEffect(new TemporaryEffect(
-                            "Parálisis",
-                            p => p.Speed = 0,
-                            p =>
-                        {
-                            if (p.HeroSelected != null)
-                            {
-                                p.Speed = p.HeroSelected.Speed;
-                            }
-                        },
-                            1
-                        ));
-                        TurnManager.MovesLeft = 0;
-                        TurnManager.NextTurn();
-                    }
-                }
-            ),
-            new Trap(
-                "Trampa de Rc Cells",
-                "Una explosión de Rc Cells del anime 'Tokyo Ghoul' que reduce la salud del jugador en 15 puntos y su velocidad en 1 punto durante 3 turnos.",
-                "/images/Traps/Red Smoke.png",
-                player =>
-                {
-                    player.Health -= 15;
-                    if (player.HeroSelected != null)
-                    {
-                        player.AddTemporaryEffect(new TemporaryEffect(
-                            "Rc Cells",
-                            p => p.Speed = Math.Max(p.Speed - 1, 0),
-                            p => p.Speed = p.HeroSelected != null ? Math.Min(p.Speed + 1, p.HeroSelected.Speed) : p.Speed,
-                            3
-                        ));
-                        TurnManager.MovesLeft--;
-                        if (TurnManager.MovesLeft <= 0)
-                        {
-                            TurnManager.NextTurn();
-                        }
-                    }
-                }
-            )
-    ];
+  public static List<Trap> Traps =
+  [
+    new Trap(
+      "Trampa de Veneno",
+      "Un gas venenoso del anime 'Naruto' que reduce la salud del jugador en 20 puntos.",
+      "/images/Traps/Green Smoke.png",
+      player => player.Health -= 20
+    ),
+    new Trap(
+      "Trampa de Parálisis",
+      "Electricidad del 'Raikiri' del anime 'Bleach' que inmoviliza al jugador por un turno.",
+      "/images/Traps/Thunder.png",
+      player =>
+      {
+        if (player != null && player.HeroSelected != null)
+        {
+          player.AddTemporaryEffect(new TemporaryEffect(
+            "Parálisis",
+            p => p.Speed = 0,
+            p =>
+            {
+              if (p.HeroSelected != null)
+              {
+                p.Speed = p.HeroSelected.Speed;
+              }
+            },
+            1
+          ));
+          TurnManager.MovesLeft = 0;
+          TurnManager.NextTurn();
+        }
+      }
+    ),
+    new Trap(
+      "Trampa de Rc Cells",
+      "Una explosión de Rc Cells del anime 'Tokyo Ghoul' que reduce la salud del jugador en 15 puntos y su velocidad en 1 punto durante 3 turnos.",
+      "/images/Traps/Red Smoke.png",
+      player =>
+      {
+        player.Health -= 15;
+        if (player.HeroSelected != null)
+        {
+          player.AddTemporaryEffect(new TemporaryEffect(
+            "Rc Cells",
+            p => p.Speed = Math.Max(p.Speed - 1, 0),
+            p => p.Speed = p.HeroSelected != null ? Math.Min(p.Speed + 1, p.HeroSelected.Speed) : p.Speed,
+            3
+          ));
+          TurnManager.MovesLeft--;
+          if (TurnManager.MovesLeft <= 0)
+          {
+            TurnManager.NextTurn();
+          }
+        }
+      }
+    )
+  ];
 }
--- Models/Trap.cs
+++ Models/Trap.cs
@@ -1,20 +1,20 @@
 namespace AnimeMaze.Models;
 public class Trap(string name, string description, string image, Action<Player> activate)
 {
-    public string Name { get; set; } = name;
-    public string Description { get; set; } = description;
-    public string Image { get; set; } = image;
-    public Action<Player> Activate { get; set; } = (player) =>
-      {
-          if (!player.TemporaryEffects.Any(e => e.Name == name))
-          {
-              activate(player);
-              player.AddTemporaryEffect(new TemporaryEffect(
-                    name,
-                    p => { /* Aplicar efecto al inicio */ },
-                    p => { /* Remover efecto al final */ },
-                    0 // Duración en TURNOS COMPLETOS
-                ));
-          }
-      };
+  public string Name { get; set; } = name;
+  public string Description { get; set; } = description;
+  public string Image { get; set; } = image;
+  public Action<Player> Activate { get; set; } = (player) =>
+  {
+    if (!player.TemporaryEffects.Any(e => e.Name == name))
+    {
+      activate(player);
+      player.AddTemporaryEffect(new TemporaryEffect(
+          name,
+          p => { /* Aplicar efecto al inicio */ },
+          p => { /* Remover efecto al final */ },
+          0 // Duración en TURNOS COMPLETOS
+      ));
+    }
+  };
 }
